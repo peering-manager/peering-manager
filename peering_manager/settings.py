f1d427448e3fd8497829a01befab5d2fa5971f61
Guillaume Mazoyer <gmazoyer@gravitons.in>
2017-09-26 23:20:14 +0200

    Move context_processors to utils app.

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -74,7 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'peering.context_processors.settings',
+                'utils.context_processors.settings',
             ],
         },
     },
