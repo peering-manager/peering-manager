dd8a50bf05dfe04e0a7f445240f5060b6f92c28f
Guillaume Mazoyer <gmazoyer@gravitons.in>
2018-07-29 17:15:31 +0200

    Remove Python 2 support from upgrade script.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,27 +1,7 @@
 #!/bin/bash
 
-# Prefer python3 and pip3 but fallback to python and pip if needed
 PYTHON="python3"
 PIP="pip3"
-type ${PYTHON} >/dev/null 2>&1 && type ${PIP} >/dev/null 2>&1 || PYTHON="python" PIP="pip"
-while getopts ":23" opt; do
-  case $opt in
-    2)
-      PYTHON="python"
-      PIP="pip"
-      echo "Forcing python/pip version 2"
-      ;;
-    3)
-      PYTHON="python3"
-      PIP="pip3"
-      echo "Forcing python/pip version 3"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
-  esac
-done
 
 # Delete old Python bytecode
 COMMAND="find . -name \"*.pyc\" -delete"
