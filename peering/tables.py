8a8ed50202986b8433d789dffe2bd0c8b2d66a41
Raúl Garcia Sanchez <info@raulgarcia.de>
2018-11-04 20:03:22 +0100

    Update peering/tables.py
    format document correction

--- peering/tables.py
+++ peering/tables.py
@@ -56,7 +56,7 @@ class CommunityTable(BaseTable):
 
     class Meta(BaseTable.Meta):
         model = Community
-        fields = ('pk', 'name', 'value', 'type', 'actions',)
+        fields = ('pk', 'name', 'value', 'type', 'actions', )
 
 
 class ConfigurationTemplateTable(BaseTable):
