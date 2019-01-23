f435509e15a5e660ae2ab1c6f9c51bc457a9e9c2
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-01-23 22:18:25 +0100

    Start user interface rework, use a sidebar and a navbar.

--- project-static/js/base.js
+++ project-static/js/base.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
   // Trigger popover on hover
   $('.popover-hover').popover({trigger: 'hover'});
+
+
+  // Toggle icon when a submenu is clicked
+  function toggleIcon(e) {
+    icon = $(e.target).prev().find('.submenu-icon');
+    if (icon.html().indexOf('caret-right') !== -1) {
+      icon.html('<i class="fas fa-caret-down"></i>');
+    } else {
+      icon.html('<i class="fas fa-caret-right"></i>');
+    }
+  }
+  $('.collapse').on('hidden.bs.collapse', toggleIcon);
+  $('.collapse').on('shown.bs.collapse', toggleIcon);
+
+  $('.list-group-item.active').click();
 });
