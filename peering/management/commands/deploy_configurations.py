8b1980a9f7d552945100f8e595bd942a95f3bc58
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-04-06 14:25:15 +0200

    Closes #111: deploy config only it has changed.

--- peering/management/commands/deploy_configurations.py
+++ peering/management/commands/deploy_configurations.py
@@ -6,23 +6,24 @@ from peering.models import InternetExchange
 
 
 class Command(BaseCommand):
-    help = (
-        "Deploy configurations each IX having a router and a configuration"
-        " template attached."
-    )
+    help = "Deploy configurations each IX having a router and a configuration template attached."
     logger = logging.getLogger("peering.manager.peering")
 
     def handle(self, *args, **options):
         self.logger.info("Deploying configurations...")
 
         for ix in InternetExchange.objects.all():
-            # Only deploy config if there are at least a configuration
-            # template, a router and a platform for the router
+            # Only deploy config if there are at least a configuration template, a
+            # router and a platform for the router
             if ix.configuration_template and ix.router and ix.router.platform:
                 self.logger.info("Deploying configuration on {}".format(ix.name))
-                ix.router.set_napalm_configuration(
-                    ix.generate_configuration(), commit=True
-                )
+
+                # Generate configuration and deploy it if it changes something
+                # (without any errors)
+                configuration = ix.generate_configuration()
+                error, changes = ix.router.set_napalm_configuration(configuration)
+                if not error and changes:
+                    ix.router.set_napalm_configuration(configuration, commit=True)
             else:
                 self.logger.info("No configuration to deploy on {}".format(ix.name))
 
