a1dab5190af1666fb88c265e8fb0a3164bf6e07c
Timothy Nowaczyk <tnowaczyk@allpointsbroadband.com>
2019-04-22 15:09:36 -0400

    added git commit tracking in the version number

--- docker/configuration.py
+++ docker/configuration.py
@@ -9,6 +9,7 @@ ALLOWED_HOSTS = ["*"]
 # Must be unique to each setup (CHANGE IT!).
 SECRET_KEY = "ef7npku*djrj_r4jt4cojo8^j@2($$@05e(eq_mn!ywx*jg0vy"
 
+VERSION = "v0.99-dev ($Id$)"
 
 BASE_PATH = ""
 
