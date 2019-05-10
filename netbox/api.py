85951bed718bb3ec46f47bf8df9c46a530b85eec
Chip Marshall <chip@2bithacker.net>
2019-05-10 13:57:43 -0700

    Fix debugging and test for NETBOX_API

--- netbox/api.py
+++ netbox/api.py
@@ -14,19 +14,22 @@ class NetBox(object):
 
     logger = logging.getLogger("peering.manager.netbox")
 
-    # pynetbox adds /api on its own. strip it off here to maintain
-    # backward compatibility with earlier Peering Manager behavior
-    base_url = settings.NETBOX_API.strip("/")
-    if base_url.endswith("/api"):
-        base_url = base_url[:-3]
-
-    api = pynetbox.api(base_url, token=settings.NETBOX_API_TOKEN)
+    api = None
+    if settings.NETBOX_API:
+        # pynetbox adds /api on its own. strip it off here to maintain
+        # backward compatibility with earlier Peering Manager behavior
+        base_url = settings.NETBOX_API.strip("/")
+        if base_url.endswith("/api"):
+            base_url = base_url[:-3]
+        api = pynetbox.api(base_url, token=settings.NETBOX_API_TOKEN)
 
     def get_devices(self):
         """
         Return all devices found with the NetBox API.
         """
-        self.logger.debug("calling api: %s | %s", api_url, search)
+        self.logger.debug(
+            "calling dcim.devices.filter: role=%s", settings.NETBOX_DEVICE_ROLES
+        )
         result = self.api.dcim.devices.filter(role=settings.NETBOX_DEVICE_ROLES)
 
         if not result:
@@ -38,6 +41,8 @@ class NetBox(object):
         """
         Runs the given NAPALM method on the device via the NetBox API.
         """
+        self.logger.debug("calling dcim.devices.get: %d", device_id)
         device = self.api.dcim.devices.get(device_id)
+        self.logger.debug("calling napalm: %s", method)
         result = device.napalm.list(method=method)
         return result[method]
