4dca552cf071ce8becd7b2e0fb2f508757b4ef5e
Chip Marshall <chip@2bithacker.net>
2019-06-18 13:28:50 -0700

    Change DEFAULT_PAGINATION_CLASS

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -130,7 +130,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": ["peering_manager.api.TokenPermissions"],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": PAGINATE_COUNT,
 }
 
