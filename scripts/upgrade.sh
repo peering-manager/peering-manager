4a80cbee739a6a5af40175528924e10a222c6488
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-08-01 22:34:19 +0200

    Remove pytz from requirements.

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -3,11 +3,6 @@
 PYTHON="python3"
 PIP="pip3"
 
-# Delete old Python bytecode
-COMMAND="find . -name \"*.pyc\" -delete"
-echo "Cleaning up old Python bytecode (${COMMAND})..."
-eval ${COMMAND}
-
 # Install and update dependencies
 COMMAND="${PIP} install -r requirements.txt --upgrade"
 echo "Updating required Python dependencies (${COMMAND})..."
