01691353f7f8eb99b1f5d4cfad427178a0bcc912
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-11-05 21:47:50 +0100

    Fix random test failures (hopefully).

--- utils/middleware.py
+++ utils/middleware.py
@@ -2,10 +2,11 @@ import sys
 import threading
 import uuid
 
+from copy import deepcopy
 from datetime import timedelta
 
 from django.db import ProgrammingError
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_save, pre_delete
 from django.conf import settings
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
@@ -36,12 +37,12 @@ def cache_changed_object(instance, **kwargs):
     local_thread.changed_objects.append((instance, action))
 
 
-def cache_deleted_object(request, instance, **kwargs):
+def cache_deleted_object(instance, **kwargs):
     """
     Record a deleted object as an object change.
     """
-    if hasattr(instance, "log_change"):
-        instance.log_change(request.user, request.id, OBJECT_CHANGE_ACTION_DELETE)
+    copy = deepcopy(instance)
+    local_thread.changed_objects.append((copy, OBJECT_CHANGE_ACTION_DELETE))
 
 
 class ExceptionCatchingMiddleware(object):
@@ -94,18 +95,18 @@ class ObjectChangeMiddleware(object):
         # Assign an ID to the given request in case we have to handle multiple objects
         request.id = uuid.uuid4()
 
-        # Listen for objects being saved (created/updated)
+        # Listen for objects being saved (created/updated) and deleted
         post_save.connect(cache_changed_object, dispatch_uid="log_object_being_changed")
-
-        # Curry context to inform the function, to be called on delete, about the
-        # request's context and listen for objects being deleted
-        cache_deleted_object_with_context = curry(cache_deleted_object, request)
-        post_delete.connect(
-            cache_deleted_object_with_context, dispatch_uid="log_object_being_deleted"
+        pre_delete.connect(
+            cache_deleted_object, dispatch_uid="log_object_being_deleted"
         )
 
         # Process the request
         response = self.get_response(request)
+
+        # Nothing to do as there are no changes to process
+        if not local_thread.changed_objects:
+            return response
 
         # Record change for each object that need to be tracked
         for changed_object, action in local_thread.changed_objects:
