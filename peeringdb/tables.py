5971ed5214e936ffdbc5f68f873c2384954fd88d
Guillaume Mazoyer <guillaume@mazoyer.eu>
2019-11-20 23:45:31 +0100

    Fix django-tables2 deprecation warnings.

--- peeringdb/tables.py
+++ peeringdb/tables.py
@@ -11,22 +11,22 @@ class PeerRecordTable(BaseTable):
 
     empty_text = "No available peers found."
     pk = SelectColumn()
-    asn = tables.Column(verbose_name="ASN", accessor="network.asn")
-    name = tables.Column(verbose_name="AS Name", accessor="network.name")
+    asn = tables.Column(verbose_name="ASN", accessor="network__asn")
+    name = tables.Column(verbose_name="AS Name", accessor="network__name")
     irr_as_set = tables.Column(
-        verbose_name="IRR AS-SET", accessor="network.irr_as_set", orderable=False
+        verbose_name="IRR AS-SET", accessor="network__irr_as_set", orderable=False
     )
     ipv6_max_prefixes = tables.Column(
-        verbose_name="IPv6", accessor="network.info_prefixes6"
+        verbose_name="IPv6", accessor="network__info_prefixes6"
     )
     ipv4_max_prefixes = tables.Column(
-        verbose_name="IPv4", accessor="network.info_prefixes4"
+        verbose_name="IPv4", accessor="network__info_prefixes4"
     )
     ipv6_address = tables.Column(
-        verbose_name="IPv6 Address", accessor="network_ixlan.ipaddr6"
+        verbose_name="IPv6 Address", accessor="network_ixlan__ipaddr6"
     )
     ipv4_address = tables.Column(
-        verbose_name="IPv4 Address", accessor="network_ixlan.ipaddr4"
+        verbose_name="IPv4 Address", accessor="network_ixlan__ipaddr4"
     )
 
     class Meta(BaseTable.Meta):
