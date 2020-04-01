2bf61138269806a5d417ca6957023ab8754b32d9
Michael Hobl <michael@hobl.com.au>
2020-04-01 18:30:50 +1000

    First-Time Credentials Defined by ENV

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,17 +2,19 @@
 
 ## Attempt connection to Postgres DB
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h $DATABASE_HOST -U $POSTGRES_USER -c '\q'; do
-  >&2 echo "Waiting for Postgres..."
-  sleep 1
+    >&2 echo "Waiting for Postgres..."
+    sleep 1
 done
 >&2 echo "Postgres online."
 
 ## Create initial user on first run
 if [ ! -f "/opt/peering-manager/has_run" ]; then
+    >&2 echo "Executing First-Run..."
     python3 manage.py migrate
     python3 manage.py collectstatic --no-input
-    python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', email='admin@example.com', password='password')"
+    python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DB_ADMIN_USER', email='', password='$DB_ADMIN_PASS')"
     touch /opt/peering-manager/has_run
+    >&2 echo "First-Run complete."
 fi
 
 exec "$@"
