af947ef07021d22da7124ec7b6f9bf23e9b93397
Guillaume Mazoyer <guillaume@mazoyer.eu>
2020-05-24 21:45:04 +0200

    Add get/set/delete functions to `UserPreferences`

--- users/models.py
+++ users/models.py
@@ -66,6 +66,81 @@ class UserPreferences(models.Model):
         ordering = ["user"]
         verbose_name = verbose_name_plural = "User Preferences"
 
+    def get(self, path, default=None, separator="."):
+        """
+        Retrieves a value based on its path. Each category and value are separated by
+        a separator (a dot by default). If the value is not found a provided default
+        will be returned instead (None by default).
+        """
+        data = self.data
+        keys = path.split(separator)
+
+        for key in keys:
+            if type(data) == dict and key in data:
+                # Step by step down the dicts
+                data = data.get(key)
+            else:
+                # Not found
+                return default
+
+        return data
+
+    def set(self, path, value, separator=".", commit=False):
+        """
+        Sets a preference value based on its path. If the preference is nested inside
+        categories and subcategories, these will be created in the process. If the
+        preference already exists its value will be overwritten. Categories and
+        subcategories cannot be overwriten with values, a TypeError exception will be
+        raised in that case. This behavior is the same for preferences that cannot be
+        changed to categories later on.
+
+        This function does not commit changes to the database if True is not provided
+        as value for the commit named parameter.
+        """
+        data = self.data
+        keys = path.split(separator)
+
+        # Look only for categories for now, excluding the name of the value
+        for i, key in enumerate(keys[:-1]):
+            if key in data and type(data[key]) == dict:
+                # Step by step down the dicts
+                data = data[key]
+            elif key in data:
+                wrong_path = separator.join(path.split(separator)[: i + 1])
+                raise TypeError(f"Cannot convert category '{wrong_path}' to a value.")
+            else:
+                # Create a new category
+                data = data.setdefault(key, {})
+
+        # Now set the actual value of the preference
+        key = keys[-1]
+        if key in data and type(data[key]) == dict:
+            raise TypeError(
+                f"'{path}' is a category, it cannot be converted to a value."
+            )
+        else:
+            data[key] = value
+
+        if commit:
+            self.save()
+
+    def delete(self, path, separator=".", commit=False):
+        data = self.data
+        keys = path.split(separator)
+
+        # Look only for categories for now, excluding the name of the value
+        for i, key in enumerate(keys[:-1]):
+            if key in data and type(data[key]) == dict:
+                # Step by step down the dicts
+                data = data[key]
+
+        # Now delete the preference
+        key = keys[-1]
+        del data[key]
+
+        if commit:
+            self.save()
+
 
 @receiver(post_save, sender=User)
 def create_userpreferences(instance, created, **kwargs):
---
+++ users/tests/test_models.py
@@ -0,0 +1,74 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from users.models import UserPreferences
+
+
+class UserPreferencesTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username="testuser")
+        user.preferences.data = {
+            "a": True,
+            "b": {"test": 1, "foo": 2},
+            "c": {"test": {"x": 10}, "foo": {"y": 11}, "bar": {"z": 12}},
+        }
+        user.preferences.save()
+        self.preferences = user.preferences
+
+    def test_get(self):
+        # Retrieve root and nested values
+        self.assertEqual(self.preferences.get("a"), True)
+        self.assertEqual(self.preferences.get("b.test"), 1)
+        self.assertEqual(self.preferences.get("c.bar.z"), 12)
+
+        # Invalid values should return None by default
+        self.assertIsNone(self.preferences.get("invalid"))
+        self.assertIsNone(self.preferences.get("a.invalid"))
+        self.assertIsNone(self.preferences.get("b.test.x.invalid"))
+        self.assertIsNone(self.preferences.get("b.test.invalid"))
+
+        # Invalid values with a non-None default should return it
+        self.assertEqual(self.preferences.get("invalid", 42), 42)
+        self.assertEqual(self.preferences.get("a.invalid", 42), 42)
+        self.assertEqual(self.preferences.get("b.test.invalid", 42), 42)
+        self.assertEqual(self.preferences.get("b.test.x.invalid", 42), 42)
+
+    def test_set(self):
+        preferences = self.preferences
+
+        preferences.set("a", "abc")
+        preferences.set("c.test.x", "xyz")
+        self.assertEqual(preferences.data["a"], "abc")
+        self.assertEqual(preferences.data["c"]["test"]["x"], "xyz")
+
+        preferences.set("d", 42)
+        preferences.set("b.bar", "value")
+        self.assertEqual(preferences.data["d"], 42)
+        self.assertEqual(preferences.data["b"]["bar"], "value")
+
+        # Commit a change to the database
+        preferences.set("a", 42, commit=True)
+        preferences.refresh_from_db()
+        self.assertEqual(preferences.data["a"], 42)
+
+        # Try to change a category into a value
+        with self.assertRaises(TypeError):
+            preferences.set("b", 42)
+
+        # Try to change a value into a category
+        with self.assertRaises(TypeError):
+            preferences.set("a.test", 42)
+
+    def test_delete(self):
+        preferences = self.preferences
+
+        # Delete existing values
+        preferences.delete("a")
+        preferences.delete("b.foo")
+        self.assertTrue("a" not in preferences.data)
+        self.assertTrue("foo" not in preferences.data["b"])
+        self.assertEqual(preferences.data["b"]["test"], 1)
+
+        # Try to delete an invalid value
+        with self.assertRaises(KeyError):
+            preferences.delete("invalid")
