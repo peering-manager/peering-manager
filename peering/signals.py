572ec7c121556d92a6f8778c1d68910b96c7670c
Guillaume Mazoyer <guillaume@mazoyer.eu>
2020-07-24 22:48:35 +0200

    Invalidate cached configuration if router or template change

--- peering/signals.py
+++ peering/signals.py
@@ -1,7 +1,13 @@
-from django.db.models.signals import pre_save
+from cacheops import CacheMiss, cache
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
-from .models import DirectPeeringSession, InternetExchangePeeringSession
+from .models import (
+    DirectPeeringSession,
+    InternetExchangePeeringSession,
+    Router,
+    Template,
+)
 
 
 @receiver(pre_save, sender=DirectPeeringSession)
@@ -31,3 +37,27 @@ def alter_internet_exchange_peering_session(instance, **kwargs):
         instance.encrypt_password(
             instance.internet_exchange.router.platform, commit=False
         )
+
+
+@receiver(post_save, sender=Router)
+def invalidate_router_cached_configuration(instance, **kwargs):
+    cached_config_name = f"configuration_router_{instance.pk}"
+    try:
+        # Unset configuration cache if router instance is changed
+        cache.get(cached_config_name)
+        cache.delete(cached_config_name)
+    except CacheMiss:
+        pass
+
+
+@receiver(post_save, sender=Template)
+def invalidate_cached_configuration_by_template(instance, **kwargs):
+    # Unset configuration for each router using the changed template
+    for router in Router.objects.filter(configuration_template=instance):
+        cached_config_name = f"configuration_router_{router.pk}"
+        try:
+            # Unset configuration cache if router instance is changed
+            cache.get(cached_config_name)
+            cache.delete(cached_config_name)
+        except CacheMiss:
+            pass
