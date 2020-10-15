28c2f1b5af050314bef915212f33e8cead683b1c
Milton Ngan <milton@valvesoftware.com>
2020-10-14 21:39:14 -0700

    Missed one more file to format with black

--- peering/management/commands/purge_deleted_objects.py
+++ peering/management/commands/purge_deleted_objects.py
@@ -11,6 +11,7 @@ from django.core.management.base import BaseCommand
 from utils.models import SoftDeleteModel
 from safedelete import HARD_DELETE
 
+
 class MinAgeAction(Action):
     def __call__(self, parser, namespace, values, option_string=None):
         if values < 0:
@@ -33,12 +34,17 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        self.logger.info(f"Purging objects at least {options['age']} days old")       
+        self.logger.info(f"Purging objects at least {options['age']} days old")
         now = datetime.datetime.now(tz=pytz.timezone(settings.TIME_ZONE))
-        expiry_date = now - datetime.timedelta(days=options['age'])
+        expiry_date = now - datetime.timedelta(days=options["age"])
         self.logger.info(f"Objects deleted before {expiry_date} will be purged")
-        model_classes = [x[1] for x in inspect.getmembers(peering.models, inspect.isclass) if issubclass(x[1], SoftDeleteModel) and not x[1]._meta.abstract ]
+        model_classes = [
+            x[1]
+            for x in inspect.getmembers(peering.models, inspect.isclass)
+            if issubclass(x[1], SoftDeleteModel) and not x[1]._meta.abstract
+        ]
         for model in model_classes:
-            count, _ = model.deleted_objects.filter(deleted__lte=expiry_date).delete(HARD_DELETE)
+            count, _ = model.deleted_objects.filter(deleted__lte=expiry_date).delete(
+                HARD_DELETE
+            )
             self.logger.info(f"Purged {count} {model._meta.verbose_name} objects")
-            
