0865ccb6e8206c073bd7948b2fb2f74b9381b465
Guillaume Mazoyer <guillaume@mazoyer.eu>
2020-12-30 20:53:10 +0100

    Closes #339: Don't try to setup invalid columns

--- utils/tables.py
+++ utils/tables.py
@@ -88,7 +88,7 @@ class BaseTable(tables.Table):
                     self.columns.show(name)
                 else:
                     self.columns.hide(name)
-            self.sequence = columns
+            self.sequence = [c for c in columns if c in self.base_columns]
 
             # Always include PK as first column
             if pk:
