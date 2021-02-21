16b40cd6a0c85f39c6d1665f6a2649583369029c
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-02-21 23:40:56 +0100

    Resolve merge conflicts

--- peering/migrations/0069_auto_20210212_1850.py
+++ peering/migrations/0069_auto_20210212_1850.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("devices", "0001_initial"),
-        ("peering", "0067_auto_20210112_1318"),
+        ("peering", "0068_router_device_state"),
     ]
 
     operations = [
