701d76e26a83b268a7cbf60b2c736868459c2d25
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-07-28 00:13:53 +0200

    Handle error feedback for API requests

--- utils/api/__init__.py
+++ utils/api/__init__.py
@@ -1,3 +1,11 @@
+import platform
+import sys
+
+from django.conf import settings
+from django.http import JsonResponse
+from django.urls import reverse
+from rest_framework import status
+
 from peering_manager.api.exceptions import SerializerNotFound
 
 
@@ -22,3 +30,25 @@ def get_serializer_for_model(model, prefix="", suffix=""):
         raise SerializerNotFound(
             f"Could not determine serializer for {app_name}.{model_name} with prefix '{prefix}' and suffix '{suffix}'"
         )
+
+
+def is_api_request(request):
+    """
+    Return `True` of the request is being made via the REST API.
+    """
+    api_path = reverse("api-root")
+    return request.path_info.startswith(api_path)
+
+
+def rest_api_server_error(request, *args, **kwargs):
+    """
+    Handle exceptions and return a useful error message for REST API requests.
+    """
+    type_, error, _ = sys.exc_info()
+    data = {
+        "error": str(error),
+        "exception": type_.__name__,
+        "peering_manager_version": settings.VERSION,
+        "python_version": platform.python_version(),
+    }
+    return JsonResponse(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
