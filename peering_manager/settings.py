168b47e6dc465fe98fff6e3dc357966854a8f590
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-10-08 22:42:34 +0200

    Post release version bump

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -17,7 +17,7 @@ HOSTNAME = platform.node()
 BASE_DIR = Path(__file__).resolve().parent.parent
 DOCS_DIR = BASE_DIR / "docs"
 
-VERSION = "v1.4.5"
+VERSION = "v1.4.6-dev"
 
 if platform.python_version_tuple() < ("3", "6"):
     raise RuntimeError(
