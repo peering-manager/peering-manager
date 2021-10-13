8e3e6654628be93ddea5ac6e9c299f4ba2849939
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-10-13 12:26:18 +0200

    Convert to int to check

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -20,7 +20,7 @@ DOCS_DIR = BASE_DIR / "docs"
 VERSION = "v1.4.6-dev"
 
 major, minor, _ = platform.python_version_tuple()
-if (major, minor) < (3, 6):
+if (int(major), int(minor)) < (3, 6):
     raise RuntimeError(
         f"Peering Manager requires Python 3.6 or higher (current: Python {platform.python_version()})"
     )
