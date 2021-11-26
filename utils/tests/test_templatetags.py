46c3710b8bc25a492ac8aee19c94a9fd9061f843
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-11-26 23:35:58 +0100

    Fix use of deprecated test method

--- utils/tests/test_templatetags.py
+++ utils/tests/test_templatetags.py
@@ -10,32 +10,32 @@ class TemplateTagsTestCase(TestCase):
         cls.a_s = AutonomousSystem.objects.create(asn=64520, name="Useless")
 
     def test_boolean_as_icon(self):
-        self.assertEquals(
+        self.assertEqual(
             '<i class="fas fa-check text-success"></i>', boolean_as_icon(True)
         )
-        self.assertEquals(
+        self.assertEqual(
             '<i class="fas fa-times text-danger"></i>', boolean_as_icon(False)
         )
 
     def test_get_status(self):
-        self.assertEquals("danger", get_status("delete"))
-        self.assertEquals("danger", get_status("REMOVE"))
-        self.assertEquals("warning", get_status("change"))
-        self.assertEquals("success", get_status("add"))
-        self.assertEquals("info", get_status("whatever"))
+        self.assertEqual("danger", get_status("delete"))
+        self.assertEqual("danger", get_status("REMOVE"))
+        self.assertEqual("warning", get_status("change"))
+        self.assertEqual("success", get_status("add"))
+        self.assertEqual("info", get_status("whatever"))
 
     def test_as_link(self):
-        self.assertEquals("Undefined", as_link("Undefined"))
+        self.assertEqual("Undefined", as_link("Undefined"))
         self.assertIn("href", as_link(self.a_s))
 
     def test_render_bandwidth_speed(self):
-        self.assertEquals("1 Tbps", render_bandwidth_speed(1000000))
-        self.assertEquals("1 Gbps", render_bandwidth_speed(1000))
-        self.assertEquals("100 Mbps", render_bandwidth_speed(100))
+        self.assertEqual("1 Tbps", render_bandwidth_speed(1000000))
+        self.assertEqual("1 Gbps", render_bandwidth_speed(1000))
+        self.assertEqual("100 Mbps", render_bandwidth_speed(100))
 
     def test_render_none(self):
-        self.assertEquals(1234, render_none(1234))
-        self.assertEquals("1234", render_none("1234"))
+        self.assertEqual(1234, render_none(1234))
+        self.assertEqual("1234", render_none("1234"))
         self.assertIn("&mdash;", render_none(""))
         self.assertIn("&mdash;", render_none(None))
         self.assertIn("href", render_none(self.a_s))
@@ -51,14 +51,14 @@ class TemplateTagsTestCase(TestCase):
         self.assertFalse(notcontains("test", "a,t"))
 
     def test_markdown(self):
-        self.assertEquals("<p>Title</p>", markdown("Title"))
-        self.assertEquals("<h1>Title</h1>", markdown("# Title"))
-        self.assertEquals(
+        self.assertEqual("<p>Title</p>", markdown("Title"))
+        self.assertEqual("<h1>Title</h1>", markdown("# Title"))
+        self.assertEqual(
             "&lt;h1&gt;Title&lt;/h1&gt;", markdown("# Title", escape_html=True)
         )
 
     def test_render_json(self):
-        self.assertEquals(
+        self.assertEqual(
             """{
     "foo": "bar"
 }""",
@@ -66,9 +66,9 @@ class TemplateTagsTestCase(TestCase):
         )
 
     def test_title_with_uppers(self):
-        self.assertEquals("Title", title_with_uppers("Title"))
-        self.assertEquals("Title", title_with_uppers("title"))
-        self.assertEquals("Title Title", title_with_uppers("Title title"))
+        self.assertEqual("Title", title_with_uppers("Title"))
+        self.assertEqual("Title", title_with_uppers("title"))
+        self.assertEqual("Title Title", title_with_uppers("Title title"))
 
     def test_doc_version(self):
         self.assertTrue("latest", doc_version("v1.5.0-dev"))
