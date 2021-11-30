78257a59e2ad8633d6a1ba08b9acfc47771eca48
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-12-01 00:01:47 +0100

    Fix WSGI deamon initialization

--- peering_manager/__init__.py
+++
@@ -1,1 +0,0 @@
-import peering_manager.schema
--- peering_manager/wsgi.py
+++ peering_manager/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peering_manager.settings")
 
 application = get_wsgi_application()
+
+import peering_manager.schema
