0bd7c03469438d5d5002d5f81246b10391be525f
Guillaume Mazoyer <guillaume@mazoyer.eu>
2021-12-08 21:33:17 +0100

    Closes #531: Fail from validation on IX-API issue

--- extras/forms.py
+++ extras/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from requests.exceptions import HTTPError
 
 from utils.forms import (
     APISelectMultiple,
@@ -19,6 +21,28 @@ class IXAPIForm(BootstrapMixin, forms.ModelForm):
     class Meta:
         model = IXAPI
         fields = ("name", "url", "api_key", "api_secret", "identity")
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        ixapi = IXAPI(
+            url=cleaned_data["url"],
+            api_key=cleaned_data["api_key"],
+            api_secret=cleaned_data["api_secret"],
+        )
+        try:
+            # Try to query API and see if it raises an error
+            ixapi.get_customers()
+        except HTTPError as e:
+            # Fail form validation on HTTP error to provide a feedback to the user
+            message = f"Unable to connect to IX-API ({e.response.status_code} {e.response.reason})"
+            if e.response.status_code >= 400 and e.response.status_code < 500:
+                possible_issue = "make sure the URL, key and secret are correct"
+            else:
+                possible_issue = "the server is malfunctioning or unavailable"
+            raise ValidationError(
+                f"Unable to connect to IX-API ({e.response.status_code} {e.response.reason}), {possible_issue}."
+            )
 
 
 class IXAPIFilterForm(BootstrapMixin, forms.Form):
