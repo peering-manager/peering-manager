d24ab5ca10226980d851a80120f1f908340bde0f
Guillaume Mazoyer <guillaume@mazoyer.eu>
2022-04-28 22:01:33 +0200

    Escape HTML chars for config diffs

--- project-static/js/base.js
+++ project-static/js/base.js
@@ -1,4 +1,10 @@
 var PeeringManager = {
+  escapeHTML: function (unsafe) {
+    return unsafe.replace(
+      /[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u00FF]/g,
+      c => '&#' + ('000' + c.charCodeAt(0)).slice(-4) + ';'
+    )
+  },
   setWorkingButton: function (button, text = "Working") {
     button.attr('disabled', 'disabled');
     button.removeClass('btn-primary').addClass('btn-warning');
