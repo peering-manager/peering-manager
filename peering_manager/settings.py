39032052fd554e82e32a469d33785ac3c6c330f1
Guillaume Mazoyer <guillaume@mazoyer.eu>
2023-04-27 21:59:45 +0200

    Add DRF FormParser for internal API calls

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "peering_manager.api.pagination.OptionalLimitOffsetPagination",
     "DEFAULT_PARSER_CLASSES": (
         "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
         "rest_framework.parsers.MultiPartParser",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
