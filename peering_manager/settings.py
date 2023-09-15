8a939c38db552a0a4780cb6d932f329ae02b96ec
Guillaume Mazoyer <guillaume@mazoyer.eu>
2023-09-15 22:02:28 +0200

    Post release version bump

--- peering_manager/settings.py
+++ peering_manager/settings.py
@@ -19,7 +19,7 @@ HOSTNAME = platform.node()
 BASE_DIR = Path(__file__).resolve().parent.parent
 DOCS_DIR = BASE_DIR / "docs"
 
-VERSION = "1.8.0"
+VERSION = "1.8.1-dev"
 
 major, minor, _ = platform.python_version_tuple()
 if (int(major), int(minor)) < (3, 8):
