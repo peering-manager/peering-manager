227d91c40978614b84b2a01bfdf7f62af8e6a1dd
Jeroen van Bemmel <jvb127@gmail.com>
2023-10-28 02:27:26 +0000

    Add note on design consideration

--- devices/crypto/nokia.py
+++ devices/crypto/nokia.py
@@ -1,4 +1,17 @@
 import bcrypt
+#
+# Implements a custom AES256 based password encryption scheme
+#
+# Note: router must be configured accordingly:
+# /admin system security hash-control custom-hash algorithm aes256 key "09123456789012345678901234567890"
+#
+# Considered generating a unique random key for each router, but it would still have to 
+# be embedded in the same config file -> offers little additional security, just more 
+# complexity
+#
+
+# KEY = hashlib.sha256(passphrase).digest()# returns 256 bit key
+KEY = "09123456789012345678901234567890".encode('ascii') # 32-character magic key, must match router
 
 #
 # Hashing algorithm only used for encryption, decrypt is no-op
