2769ad26d2b4fb311f723e660eac04480c64dd48
Guillaume Mazoyer <guillaume@mazoyer.eu>
2024-10-09 13:22:47 +0200

    Make reveal button smaller for password inputs

--- project-static/js/forms.js
+++ project-static/js/forms.js
@@ -163,10 +163,10 @@ $(document).ready(function () {
     inputType = $(passwordInputID).attr('type');
     if (inputType == 'password') {
       $(passwordInputID).attr('type', 'text');
-      $(this).html('<i class="fas fa-eye-slash"></i> Hide');
+      $(this).html('<i class="fa-fw fa-solid fa-eye-slash"></i>');
     } else {
       $(passwordInputID).attr('type', 'password');
-      $(this).html('<i class="fas fa-eye"></i> Show');
+      $(this).html('<i class="fa-fw fa-solid fa-eye"></i>');
     }
   });
 
