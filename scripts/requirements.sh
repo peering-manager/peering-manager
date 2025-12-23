05f0f58def79f936de3848c3431d296215c5ba6a
Guillaume Mazoyer <guillaume@mazoyer.eu>
2025-12-23 18:19:03 +0100

    Run uv export by stripping markers

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-uv export --no-hashes --no-annotate --no-header --no-dev --no-group docs --format requirements-txt >| requirements.txt
+uv export --no-hashes --no-annotate --no-header --no-dev --no-group docs --format requirements-txt | awk '{ print $1 }' FS=' ; ' >| requirements.txt
