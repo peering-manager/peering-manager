45efa0fb2bfa4b49160c6197ebb50727b21f16c6
Guillaume Mazoyer <guillaume@mazoyer.eu>
2026-06-27 13:42:12 +0200

    Add wiring to manage table config
    * Add new buttons in the table column modal to create or clear a table
      config. This applies only to user with the proper set of permissions.
     * Honour table config if they exist and use them as a first fallback
       when a user does not have any preferences defined for a table.

--- peering_manager/views/generic/mixins.py
+++ peering_manager/views/generic/mixins.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 
+from django.apps import apps
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import redirect
 
 from utils.forms import TableColumnsForm
@@ -36,6 +38,63 @@ class ActionsMixin:
 
 
 class TableMixin:
+    def _save_columns(self, request, form):
+        """Store the submitted columns as the current user's preference."""
+        request.user.preferences.set(
+            f"tables.{form.table_name}.columns",
+            form.cleaned_data["columns"],
+            commit=True,
+        )
+        messages.success(request, "Your preferences have been updated")
+
+    def _reset_columns(self, request, form):
+        """Drop the current user's column preference for the table."""
+        request.user.preferences.delete(
+            f"tables.{form.table_name}.columns", commit=True
+        )
+        messages.success(request, "Your preferences have been updated")
+
+    def _set_default_columns(self, request, form, table):
+        """Promote the submitted columns to the default applied to every user."""
+        if not request.user.has_perm("extras.change_tableconfig"):
+            messages.error(request, "You do not have permission to set default columns")
+            return
+
+        columns = form.cleaned_data["columns"]
+        if not columns:
+            messages.error(request, "At least one column must be selected as a default")
+            return
+
+        table_config = apps.get_model("extras", "TableConfig")
+        model = table._meta.model
+        table_config.objects.update_or_create(
+            table=form.table_name,
+            defaults={
+                "columns": columns,
+                "object_type": (
+                    ContentType.objects.get_for_model(model) if model else None
+                ),
+            },
+        )
+        messages.success(request, "The default columns for all users have been updated")
+
+    def _clear_default_columns(self, request, form):
+        """Remove the operator-defined default for the table, if any."""
+        if not request.user.has_perm("extras.delete_tableconfig"):
+            messages.error(
+                request, "You do not have permission to clear default columns"
+            )
+            return
+
+        table_config = apps.get_model("extras", "TableConfig")
+        deleted, _ = table_config.objects.filter(table=form.table_name).delete()
+        if deleted:
+            messages.success(
+                request, "The default columns for all users have been cleared"
+            )
+        else:
+            messages.info(request, "There was no default to clear")
+
     def get_table(self, data, request, bulk_actions=True):
         """
         Return the django-tables2 Table instance to be used for rendering the objects
@@ -53,14 +112,13 @@ class TableMixin:
         form = TableColumnsForm(table=table, data=request.POST)
 
         if form.is_valid():
-            preference = f"tables.{form.table_name}.columns"
-
             if "save" in request.POST:
-                request.user.preferences.set(
-                    preference, form.cleaned_data["columns"], commit=True
-                )
+                self._save_columns(request=request, form=form)
             elif "reset" in request.POST:
-                request.user.preferences.delete(preference, commit=True)
-            messages.success(request, "Your preferences have been updated")
+                self._reset_columns(request=request, form=form)
+            elif "set_default" in request.POST:
+                self._set_default_columns(request=request, form=form, table=table)
+            elif "clear_default" in request.POST:
+                self._clear_default_columns(request=request, form=form)
 
         return redirect(request.get_full_path())
